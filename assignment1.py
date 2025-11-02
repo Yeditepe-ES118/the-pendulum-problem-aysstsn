c7b83cfc0e7d8f34a4354a918780008dd6ef3338
“aysstsn” <ayse.tosun2@std.yeditepe.edu.tr>
2025-11-02 21:16:10 +0300

    fix error

--- assignment1.py
+++ assignment1.py
@@ -4,7 +4,7 @@ def find_period(L0, L1):
         g = 9.81 #in m/s^2
         for l in range(L0, L1+1, 1):
             t = 2 * np.pi * np.sqrt(l/g)
-            print("When L = %.1f m, T =  %.1f s" % (l, t))
+            print("When L = %.1f m, T = %.1f s" % (l, t))
         
         T0 = 2 * np.pi * np.sqrt(L0/g)
         T1= 2 * np.pi * np.sqrt(L1/g)
